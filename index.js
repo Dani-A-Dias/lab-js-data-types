3613503604379f2f69cd761fc33201057aa6c573
DaniDias95 <guiguimu_art@hotmail.com>
2024-05-08 14:00:21 +0100

    Fixed the stuff I had wrong(I hope) with AI Feedback

--- index.js
+++ index.js
@@ -22,10 +22,8 @@ const part1 = "java";
 const part2 = "script";
 
 // Convert the last letter of part1 and part2 to uppercase and concatenate the strings
-const upperPart1 = part1.slice(0,3) + part1[3].toUpperCase();
-const upperPart2 = part2.slice(0,5) + part2[5].toUpperCase();
 
-const convName = upperPart1 + upperPart2;
+const convName = part1.slice(0, -1) + part1.charAt(part1.length -1).toUpperCase() + part2.slice(0, -1) + part2.charAt(part2.length -1).toUpperCase();
 // Print the cameLtaiL-formatted string
 
 console.log(convName);
@@ -49,7 +47,7 @@ console.log(billTotalTip)
 *******************************************/
 
 // Generate a random integer between 1 and 10 (inclusive)
-let randUpTen = Math.round(Math.random()*10);
+let randUpTen = Math.ceil(Math.random()*10);
 
 // Print the generated random number
 
@@ -63,17 +61,17 @@ const a = true;
 const b = false;
 
 // Try and guess the output of the below expressions first and write your answers down:
-const expression1 = a && b; 
-//false
-const expression2 = a || b; 
-//true
-const expression3 = !a && b; 
-//true
-const expression4 = !(a && b); 
-//false
-const expression5 = !a || !b; 
-//true
-const expression6 = !(a || b); 
-//false
-const expression7 = a && a; 
-//true
+const expression1 = a && b; //false
+console.log(expression1)
+const expression2 = a || b; //true
+console.log(expression2)
+const expression3 = !a && b; //false
+console.log(expression3)
+const expression4 = !(a && b); //true
+console.log(expression4)
+const expression5 = !a || !b; //true
+console.log(expression5)
+const expression6 = !(a || b); //false
+console.log(expression6)
+const expression7 = a && a; //true
+console.log(expression7)
